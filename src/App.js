18732ae4edcc2032347fc54a2b6e5547d0640f55
Maria Gonzalez <mayigd0502@gmail.com>
2020-12-10 03:34:51 +0000

    RD-8 Implement getThisWeekWeather() method in weather service

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React, { useEffect, useState } from "react";
 import "./App.css";
 import WeatherService from "./services/weather.js";
-import LocationService from "./services/location";
+//import LocationService from "./services/location";
 function App() {
   const [temp, setTemp] = useState(0);
   const [cityName, setCityName] = useState("");
   const [country, setCountry] = useState("");
+  const [firstDay, setFirstDay] = useState([]);
+  const [secondDay, setSecondDay] = useState([]);
+  const [thirdDay, setThirdDay] = useState([]);
+  const [fourthDay, setFourthDay] = useState([]);
+  const [fifthDay, setFifthDay] = useState([]);
+  const [sixthDay, setSixthDay] = useState([]);
+  const [seventhDay, setSeventhDay] = useState([]);
+
   useEffect(() => {
     WeatherService.getCurrentWeather().then((current) => {
       setTemp(current.temp);
@@ -13,13 +21,24 @@ function App() {
       setCountry(current.country);
     });
   }, []);
-
+  useEffect(() => {
+    WeatherService.getThisWeekWeather().then((daily) => {
+      setFirstDay(daily.firstDay);
+      setSecondDay(daily.secondDay);
+      setThirdDay(daily.thirdDay);
+      setFourthDay(daily.fourthDay);
+      setFifthDay(daily.fifthDay);
+      setSixthDay(daily.sixthDay);
+      setSeventhDay(daily.seventhDay);
+    });
+  });
   return (
     <div className="App">
       <h1>
         Temperatura en {cityName}:{temp}
+        {country}
+        {console.log(firstDay)}
       </h1>
-      <h1>{console.log(LocationService.getCurrentPosition())}</h1>
     </div>
   );
 }
--- src/services/weather.js
+++ src/services/weather.js
@@ -1,7 +1,7 @@
 import LocationService from "./location";
+const apiKey = process.env.REACT_APP_MY_API_KEY;
 class WeatherService {
   async getCurrentWeather() {
-    const apiKey = process.env.REACT_APP_MY_API_KEY;
     const position = await LocationService.getCurrentPosition();
     const response = await fetch(
       "http://api.openweathermap.org/data/2.5/weather?lat=" +
@@ -20,6 +20,69 @@ class WeatherService {
       windSpeed: current.wind.speed,
       humidity: current.main.humidity,
       country: current.sys.country,
+    };
+  }
+
+  async getThisWeekWeather() {
+    const position = await LocationService.getCurrentPosition();
+    const response = await fetch(
+      "https://api.openweathermap.org/data/2.5/onecall?lat=" +
+        position.coords.latitude +
+        "&lon=" +
+        position.coords.longitude +
+        "&exclude=current,minutely,hourly,alerts&units=metric&appid=" +
+        apiKey
+    );
+    const { daily } = await response.json();
+    return {
+      currentDay: [
+        daily[0].temp,
+        daily[0].feels_like,
+        daily[0].wind_speed,
+        daily[0].humidity,
+      ],
+      firstDay: [
+        daily[1].temp,
+        daily[1].feels_like,
+        daily[1].wind_speed,
+        daily[1].humidity,
+      ],
+      secondDay: [
+        daily[2].temp,
+        daily[2].feels_like,
+        daily[2].wind_speed,
+        daily[2].humidity,
+      ],
+      thirdDay: [
+        daily[3].temp,
+        daily[3].feels_like,
+        daily[3].wind_speed,
+        daily[3].humidity,
+      ],
+      fourthDay: [
+        daily[4].temp,
+        daily[4].feels_like,
+        daily[4].wind_speed,
+        daily[4].humidity,
+      ],
+      fifthDay: [
+        daily[5].temp,
+        daily[5].feels_like,
+        daily[5].wind_speed,
+        daily[5].humidity,
+      ],
+      sixthDay: [
+        daily[6].temp,
+        daily[6].feels_like,
+        daily[6].wind_speed,
+        daily[6].humidity,
+      ],
+      seventhDay: [
+        daily[7].temp,
+        daily[7].feels_like,
+        daily[7].wind_speed,
+        daily[7].humidity,
+      ],
     };
   }
 }
