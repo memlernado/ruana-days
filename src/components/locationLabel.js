85350858b95f09b559a51f15dd2e7d846b134ba5
Santiago Gonzalez <sagodi97@gmail.com>
2021-02-27 20:08:12 +0100

    Style location label with styled-components

--- src/components/locationLabel.js
+++ src/components/locationLabel.js
@@ -1,17 +1,25 @@
 import React from "react";
+import styled from "styled-components";
+
+const LocationLabelContainer = styled.div`
+  min-width: max-content;
+  max-width: 400;
+  color: black;
+  font-weight: border;
+`;
+
 function LocationLabel(props) {
   let country;
-  if(props.city !== undefined){
-    country = <h2>{props.country}</h2>
-  }
-  else{
-    country = <h1>{props.country}</h1>
+  if (props.city !== undefined) {
+    country = <h2>{props.country}</h2>;
+  } else {
+    country = <h1>{props.country}</h1>;
   }
   return (
-    <div>
-      <h1 >{props.city}</h1>
+    <LocationLabelContainer>
+      <h1>{props.city}</h1>
       {country}
-    </div>
+    </LocationLabelContainer>
   );
 }
 export default LocationLabel;
